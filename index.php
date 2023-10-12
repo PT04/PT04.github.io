4b6966eeb594dd6d5194c5e29370c867e7017018
PT04 <tripathipavan10@gmail.com>
2023-10-12 11:02:38 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?
+$current_page = "Home";
+include("header.inc");
+include("menu.inc");
+h1 {
+  font-size: 40px;
+
+}
+
+?>
+
+
+<!-- begin content -->
+
+<img src="dp.jpg" alt="pavan photo" width="250" height="250" style="float: left; margin-right: 10px; margin-bottom: 5px; border: solid black 1px; padding: 2px" >
+<h1>Pavan Rakesh Tripathi पवन राकेश त्रिपाठी</h1>
+<p>Welcome to my webpage!<br> I am MS Research scholar in <a href="https://www.iitk.ac.in/ee/">Electrical Engineering</a> (Photonics) at Indian Institute of Technology <a href="https://www.iitk.ac.in/">(IIT) Kanpur</a>, India.</p>
+<p><a href="https://home.iitk.ac.in/~pavantr/index.php">Home</a></p>
+<p><a href="https://home.iitk.ac.in/~pavantr/work.php">Work</a></p>
+<p><a href="https://home.iitk.ac.in/~pavantr/contact.php">Contact me</a></p>
+
+<!-- end content -->
+
+<?
+include("footer.inc");
+?>
